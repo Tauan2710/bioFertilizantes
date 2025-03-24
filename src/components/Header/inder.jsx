3b6029ed2bba53797d333d27631be450b0c73af1
Tauan2710 <tauan12.udi@hotmail.com>
2025-03-24 14:02:55 -0300

    Header completa

---
+++ src/components/Header/inder.jsx
@@ -0,0 +1,11 @@
+import { HeaderContainer } from "./Header.styles";
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+        <Logo/>
+        <h1>Meu Menu</h1>
+        </HeaderContainer>
+    );
+    }
+    export default Header;
